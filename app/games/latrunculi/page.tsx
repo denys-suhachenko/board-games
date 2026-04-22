079d2d1903b583875b3f3fd4845c81d521619bb7
Denys <denys.x95@gmail.com>
2026-04-22 07:54:05 +0300

    move latrunculi game board to separate component

--- app/games/latrunculi/page.tsx
+++ app/games/latrunculi/page.tsx
@@ -1,57 +1,6 @@
-import Image from 'next/image';
-
-import { cn } from '@/shared/lib/utils';
-
-type Figure = {
-  color: 'white' | 'black';
-  type: 'disk' | 'pyramid';
-};
-
-type PieceProps = {
-  cell: Figure;
-};
-
-export function Piece({ cell }: PieceProps) {
-  let src = '';
-
-  if (cell.color === 'black') {
-    src =
-      cell.type === 'pyramid'
-        ? '/figures/pyramids/pyramid_black.svg'
-        : '/figures/disks/disk_black_shadow.svg';
-  } else {
-    src =
-      cell.type === 'pyramid'
-        ? '/figures/pyramids/pyramid_white.svg'
-        : '/figures/disks/disk_white_shadow.svg';
-  }
-
-  return <Image src={src} alt={cell.color} width={80} height={80} />;
-}
-
-function initBoard(width: number, height: number) {
-  const board = Array.from({ length: height }, () =>
-    Array<Figure | null>(width).fill(null),
-  );
-  const centerX = Math.floor(width / 2);
-
-  for (let i = 0; i < width; i++) {
-    board[0][i] = { color: 'white', type: 'disk' };
-    board[1][i] = { color: 'white', type: 'disk' };
-
-    board[height - 1][i] = { color: 'black', type: 'disk' };
-    board[height - 2][i] = { color: 'black', type: 'disk' };
-  }
-
-  board[0][centerX] = { color: 'white', type: 'pyramid' };
-  board[height - 1][centerX] = { color: 'black', type: 'pyramid' };
-
-  return board;
-}
+import { LatrunculiGameBoard } from '@/components/latrunculi_game/ui';
 
 export default function LatrunculiPage() {
-  const board = initBoard(7, 7);
-
   return (
     <div className="mx-auto w-full max-w-4xl px-6 py-8 md:px-4 md:py-8">
       <div className="mb-8 rounded-md border border-black bg-[#EFE9DC] p-2 shadow-sm">
@@ -62,71 +11,7 @@ export default function LatrunculiPage() {
         </div>
       </div>
 
-      <div className="rounded-sm bg-[#A75934] p-4 shadow-md/10">
-        <div className="border border-[#D9B06C] bg-[#A75934] p-8">
-          <div className="grid aspect-square grid-cols-7 grid-rows-7 border border-[#7A6148] bg-[#EACDA1] p-2">
-            {board.map((row, i) =>
-              row.map((cell, j) => {
-                return (
-                  <div
-                    key={`${i}-${j}`}
-                    className={cn(
-                      '-mt-px -ml-px flex items-center justify-center border border-[#7A6148] bg-[#EACDA1] select-none',
-                    )}
-                  >
-                    {cell && <Piece cell={cell} />}
-                  </div>
-                );
-              }),
-            )}
-          </div>
-        </div>
-      </div>
-
-      {/* <div className="rounded-md border border-black bg-[#EFCFAD] p-1 shadow-(--board-shadow)">
-        <div className="rounded-md border border-white/50 bg-[#8D3C1C] p-6">
-          <div className="grid aspect-square grid-cols-7 grid-rows-7 rounded-sm border border-[#221504] bg-[#562C02]">
-            {board.map((row, i) =>
-              row.map((cell, j) => {
-                return (
-                  <div
-                    key={`${i}-${j}`}
-                    className={cn(
-                      'flex items-center justify-center rounded-xs border border-[#562C02] bg-[#CE9C6B] select-none md:rounded-sm',
-                    )}
-                  >
-                    {cell && <Piece cell={cell} />}
-                  </div>
-                );
-              }),
-            )}
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div className="rounded-md border border-black bg-(--board-ceramic-border) p-2 shadow-(--board-shadow)">
-        <div className="rounded-md border border-white bg-(--board-ceramic-bg) p-2">
-          <div className="grid aspect-square grid-cols-7 grid-rows-7 bg-(--board-ceramic-cell-border)">
-            {board.map((row, i) =>
-              row.map((cell, j) => {
-                return (
-                  <div
-                    key={`${i}-${j}`}
-                    className={cn(
-                      'flex items-center justify-center rounded-xs border border-(--board-ceramic-cell-border) select-none md:rounded-sm',
-                      (i + j) % 2 === 0
-                        ? 'bg-(--board-ceramic-cell)'
-                        : 'bg-(--board-ceramic-cell-alternate)',
-                    )}
-                  >
-                    {cell && <Piece cell={cell} />}
-                  </div>
-                );
-              }),
-            )}
-          </div>
-        </div>
-      </div> */}
+      <LatrunculiGameBoard />
     </div>
   );
 }
