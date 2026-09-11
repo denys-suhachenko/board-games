88cf52f476fa10fac0b8bfb42cd6c8405b09324d
Denys Suhachenko <denys.x95@gmail.com>
2026-09-11 20:30:19 +0300

    added Sentry for errors monitoring

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,21 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://1d302e4c037a2dc21d3a81cc12963707@o4510985193390080.ingest.de.sentry.io/4512068867129424',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
